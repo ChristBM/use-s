ed8ac972d43926ec94808a4f8d2fa8f5beb5729d
Christian Boffill <christian.boffill@gmail.com>
2025-08-28 15:34:40 -0400

    feat: add loadFromLocalStorage and saveOnLocalStorage for state persistence

---
+++ src/functions/local-storage.ts
@@ -0,0 +1,38 @@
+import { FullCopy } from "full-copy";
+import type { GlobalStateConfig } from "../types";
+import { deepAssign } from "./deep-assign";
+import { isValidChange } from "./is-valid-change";
+import { isObjectWithKeys } from "./is-object-with-keys";
+import { serialize, deserialize } from "./serialize-deserialize";
+
+export function loadFromLocalStorage<T>({ value: current, key }: GlobalStateConfig<T>): T {
+  const storage = localStorage.getItem(key);
+
+  if (storage) {
+    const resolved: T = deserialize(storage);
+
+    if (!isValidChange(current, resolved)) return current;
+
+    let newState: T = resolved as T;
+
+    if (isObjectWithKeys(current) && isObjectWithKeys(resolved)) {
+      newState = FullCopy(current);
+      deepAssign(newState as object, resolved as object, true);
+    }
+
+    return newState;
+  } else return current;
+}
+
+export function saveOnLocalStorage<T>({ value, key }: GlobalStateConfig<T>) {
+  return new Promise(function (resolve) {
+    setTimeout(() => {
+      try {
+        localStorage.setItem(key, serialize(value));
+        resolve(true);
+      } catch {
+        resolve(false);
+      }
+    }, 0);
+  });
+}
