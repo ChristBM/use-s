422499f9a723ca42529f3c45b9e94a0b9f99a59c
Christian Boffill <christian.boffill@gmail.com>
2025-10-07 10:52:32 -0400

    init validation for the global state is done in isGlobalStateConfig, and persist is now extracted from init

--- src/functions/normalize-init.ts
+++ src/functions/normalize-init.ts
@@ -1,19 +1,17 @@
 import { FullCopy } from "full-copy";
 import type { GlobalStateConfig, NormalizedInit } from "../types";
-import { isKeyInitialized, createGlobalState, getGlobalState } from "../store";
+import { createGlobalState, getGlobalState, isKeyInitialized } from "../store";
 
-export function normalizeInit<T>(init: T | GlobalStateConfig<T>, { mutableIn = false, persist = false }): NormalizedInit<T> {
+export function normalizeInit<T>(init: T | GlobalStateConfig<T>, { mutableIn = false }): NormalizedInit<T> {
   if (isGlobalStateConfig<T>(init)) {
-    const { value, key } = init;
-
+    const { value, key, persist } = init;
     if (!isKeyInitialized(key)) createGlobalState<T>({ value: mutableIn ? value : FullCopy(value), key, persist });
-
-    return { initialValue: getGlobalState(key) as T, key: key && key.length > 0 ? key : undefined };
+    return { initialValue: getGlobalState(key) as T, key, persist };
   }
 
   return { initialValue: mutableIn ? init : FullCopy(init) };
 }
 
 function isGlobalStateConfig<T>(conf: unknown): conf is GlobalStateConfig<T> {
-  return typeof conf === "object" && conf !== null && "value" in conf && "key" in conf;
+  return (typeof conf === "object" && conf !== null && "value" in conf && "key" in conf && typeof conf.key === "string" && conf.key.length > 0);
 }
